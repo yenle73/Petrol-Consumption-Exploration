bced647e368c68de9de46cacea0199820e1af0c2
yenle73 <128300242+yenle73@users.noreply.github.com>
2023-11-29 08:45:07 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ st.title("Data Exploration")
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
   df = pd.read_csv(uploaded_file)
-  st.write(dataframe)
+  st.write(df)
