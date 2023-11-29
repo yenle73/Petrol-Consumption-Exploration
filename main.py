7b104d80476192fa96e9013074dca9f3aeb190b8
yenle73 <128300242+yenle73@users.noreply.github.com>
2023-11-29 08:48:45 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import pandas as pd
 
 st.title("Data Exploration")
 
+st.header('Uploading data')
 uploaded_file = st.file_uploader("Choose a file")
+
+st.header('Visualizing data')
 if uploaded_file is not None:
   df = pd.read_csv(uploaded_file)
   st.write(df)
